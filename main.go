3dcea564b92f5aa2642cf8ec6d0dee2b46a53dd6
nikhil-kanotra <33729365+librannk@users.noreply.github.com>
2022-08-08 18:16:14 +0530

    changed message

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello EKS World"}
+		res := &response{Message: "Hello EKS World!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
